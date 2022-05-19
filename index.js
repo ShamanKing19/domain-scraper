10ee0c25205326e0c061dfb91adf9a2e6dde4935
Георгий Сухомлин <georgy.sukhomlin@atos.net>
2022-05-19 10:15:28 +0200

    Убрал лишние await

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ import { userAgents } from './functions.js';
 import axios from 'axios';
 import mysql from 'mysql2';
 import 'dotenv/config';
-import https from 'http'
+import https from 'http';
 
 async function makeRequestAsync(domain, index) {
     let url = "http://" + domain;
 
-    await axios.get(url, {
-        timeout: 5000,
+    axios.post(url, {
+        timeout: 3000,
         userAgents: userAgents(),
         maxRedirects: 3,
         httpsAgent: new https.Agent({
             rejectUnauthorized: false
         })
-    }).then(function (response) {
+    }).then((response) => {
         console.log("№" + index + " " + response.config['url'] + " " + response.status);
-    }).catch(function (err) {
+    }).catch((err) => {
         console.log("№" + index + " " + url + " " + err.message);
     });
+
 }
 
-async function parseDomains() {
+function parseDomains() {
     const connection = mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
@@ -34,10 +35,12 @@ async function parseDomains() {
     connection.query(sql_query, function (error, result) {
         
         if (!error) {
+            
             //version 3
             for (let item of result) {
                 makeRequestAsync(item['domain'], ++i);
             }
+            
             //version2
             /*
             let i = 0;
@@ -45,11 +48,10 @@ async function parseDomains() {
                 await makeRequestAsync(item['domain'], ++i);
             });
             */
-    
-            //version 1
             /*
+            //version 1
             result.forEach(function (current_value, index, array) {
-                makeRequestAsync(current_value['domain'], index); // TODO: здесь await
+                makeRequestAsync(current_value['domain'], ++i); // TODO: здесь await
             });
             */
         }
