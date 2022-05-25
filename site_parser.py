dd098e57cb01043a1bb98522f848aa863f018f7a
Георгий Сухомлин <georgy.sukhomlin@atos.net>
2022-05-25 14:14:55 +0200

    Сделал определение города, области и определение категории по заголовку и описани (нужно поработать над списком ключевых слов, либо изменить определять категорию по словам на самой странице

--- site_parser.py
+++ site_parser.py
@@ -3,9 +3,6 @@ import os
 import time
 import re
 import json
-
-from requests import head
-
 try:
     import asyncio
 except:
@@ -30,6 +27,15 @@ try:
     import lxml
 except:
     os.system("pip install lxml")
+try:
+    import phonenumbers
+    from phonenumbers import geocoder
+    from phonenumbers import carrier
+    from phonenumbers import timezone
+except:
+    os.system("pip install phonenumbers")
+
+
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
@@ -49,6 +55,26 @@ def insert_into_db(connection, table_name, domain, zone, real_domain, status):
     connection.commit()
 
 
+def select_status_200_from_db(connection):
+    with connection.cursor() as cursor:
+        sql = "SELECT real_domain FROM domains WHERE status=200"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+
+
+def select_categories_from_db(connection):
+    with connection.cursor() as cursor:
+        sql = """
+                SELECT category.name, subcategory.name, tags.tag FROM category
+                RIGHT JOIN subcategory ON category.id = subcategory.category_id
+                INNER JOIN tags ON subcategory.id = tags.id
+            """
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+
+
 def get_headers():
     user_agents = {
         'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
@@ -63,7 +89,7 @@ def get_headers():
     return user_agents
 
 
-async def parse_site(domain, counter, start_time):
+async def parse_site(domain, counter, categories, start_time):
     timeout_sec = 30
     session_timeout = aiohttp.ClientTimeout(
         total=None, sock_connect=timeout_sec, sock_read=timeout_sec)
@@ -78,7 +104,7 @@ async def parse_site(domain, counter, start_time):
             print(
                 f"№{counter} - {time.time() - start_time} ----------------------------------------------------")
             print(f"URL: {url}")
-            await identify_site(html)
+            await identify_site(html, categories)
             counter += 1
     except Exception as error:
         print(error)
@@ -86,16 +112,7 @@ async def parse_site(domain, counter, start_time):
         await session.close()
 
 
-def select_from_db(connection):
-    with connection.cursor() as cursor:
-        sql = "SELECT real_domain FROM domains WHERE status=200"
-        # sql = "SELECT real_domain FROM domains WHERE status=200 LIMIT 10000"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        return result
-
-
-async def identify_site(html):
+async def identify_site(html, categories):
     bs4 = BeautifulSoup(html, "lxml")
     valid = await check_valid(bs4)
     if valid:
@@ -108,17 +125,64 @@ async def identify_site(html):
     cms = await identify_cms(bs4)
     contacts = await find_contacts(bs4)
     inn = await find_inn(bs4.text)
-    # cities = await find_cities(contacts['mobile_phones']) # не успевает схватывать и выводить номер
+    category = await identify_category(title, description, categories)
+    cities = await identify_city(contacts['mobile_numbers']) # не успевает схватывать и выводить номер
 
     print(f"Title: {title}")
     print(f"Description: {description}")
     print(f"CMS: {cms}")
     print(f"Контакты: {contacts}")
-    print(f"ИНН: {inn}\n")
-    # print(f"Город: {cities}\n")
+    print(f"ИНН: {inn}")
+    print(f"Категория: {category}")
+    print(f"Город: {cities}\n")
 
-    # print(f"Категория: {category}\n")
-    # print(f"Подкатегория: {subcategory}\n")
+
+async def identify_category(title, description, raw_categories):
+    categories = await get_categories_dict(raw_categories)
+    for category in categories:
+        for subcategory in categories[category]:
+            for keyword in categories[category][subcategory]:
+                if keyword in title or keyword in description:
+                    return {"category": category, "subcategory": subcategory}
+                # print(f'Category: {category}, Subcategory: {subcategory}, keyword: {keyword}')
+    
+
+
+# Преобразует sql запрос в нужный формат
+async def get_categories_dict(raw_categories):
+    # Получение названий категорий без дубликатов
+    category_titles = []
+    for row in raw_categories:
+        category_titles.append(row['name'])
+    category_titles = list(set(category_titles))
+
+    # Заполнение словаря заголовками
+    tags_dict = {}
+    for title in category_titles:
+        tags_dict[title] = {}
+    
+    # Заполнение словаря тэгов
+    for row in raw_categories:
+        category_name = row['name']
+        subcategory_name = row['subcategory.name']
+        tags = row['tag'].split(', ')
+        tags_dict[category_name][subcategory_name] = tags
+
+    # print(json.dumps(tags_dict, indent=4, sort_keys=True, ensure_ascii=False))
+    return tags_dict
+
+
+async def identify_city(numbers):
+    cities = []
+    for number in numbers:
+        valid_number = phonenumbers.parse(number, "RU")
+        # print(valid_number)
+        location = geocoder.description_for_number(valid_number, "ru")
+        operator = carrier.name_for_number(valid_number, "en")
+        # print(location, operator)
+        cities.append(location)
+    return list(set(cities))
+    
 
 async def find_inn(text):
     # TODO: Отсечь личшние символы вначеле и в конце, потому что схватывает рандомные числа из ссылок
@@ -129,7 +193,7 @@ async def find_inn(text):
     coefficients_10 = [2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
     coefficients_12_1 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0, 0]
     coefficients_12_2 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
-    
+
     # Тут должны быть формулы проверки корректности ИНН
     for inn in all_inns:
         if len(inn) == 10:
@@ -141,7 +205,7 @@ async def find_inn(text):
             key_value = key - (key // 11) * 11
             if key_value == int(inn[-1]):
                 correct_inns.append(inn)
-        
+
         elif len(inn) == 12:
             key1 = 0
             key2 = 0
@@ -162,6 +226,8 @@ async def find_inn(text):
 # TODO: Проверить скорость с более точным поиском
 # Находить контакты можно чаще, но медленнее
 # можно искать все классы, в которых будет phone, contacts
+
+
 async def find_contacts(bs4):
     links = bs4.findAll('a')
     mobile_numbers = []
@@ -208,7 +274,7 @@ async def check_valid(bs4):
                         'домен недоступен', 'домен временно недоступен', 'вы владелец сайта?', 'технические работы', 'сайт отключен', 'сайт заблокирован',
                         'сайт недоступен', 'это тестовый сервер', 'это тестовый сайт', 'срок регистрации', 'the site is',
                         '503 service', '404 not found', 'fatal error', 'настройте домен', 'under construction',  'не опубликован',
-                        'домен зарегистрирован', 'доступ ограничен', 'Welcome to nginx', 'owner of this ']
+                        'домен зарегистрирован', 'доступ ограничен', 'welcome to nginx', 'owner of this ']
     for keyword in invalid_keywords:
         if keyword in text.lower():
             print(f'Invalid cuz of: {keyword}\n')
@@ -235,30 +301,26 @@ async def find_title(bs4):
     return title
 
 
-async def parse_all_sites(domains):
-    # test_numbers = [79184251015, 79278921675, 89118535474]
-    # for number in test_numbers:
-    #     print(await check_phone(number))
-    
-    counter = 1
+async def parse_all_sites(domains, connection):
     start_index = 0
     step = 10000
     requests = []
     start_time = time.time()
+
+    categories = select_categories_from_db(connection)
 
     print(f"Парсинг начался")
 
     for portion in range(start_index, len(domains), step):
         for domain_index in range(start_index, portion):
             requests.append(parse_site(
-                domains[domain_index]["real_domain"], counter, start_time))
-            counter += 1
+                domains[domain_index]["real_domain"], domain_index+1, categories,  start_time))
         await asyncio.gather(*requests)
         requests.clear()
         start_index = portion
 
     print(
-        f"Парсинг {counter-1} сайтов закончился за {time.time() - start_time}")
+        f"Парсинг {domain_index+1} сайтов закончился за {time.time() - start_time}")
 
 
 def main():
@@ -268,10 +330,11 @@ def main():
     db_password = os.environ.get("DB_PASSWORD")
 
     connection = create_connection(db_host, db_user, db_password, db_name)
-    domains = select_from_db(connection)  # domains with status = 200
+    domains = select_status_200_from_db(
+        connection)  # domains with status = 200
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(parse_all_sites(domains))
+    loop.run_until_complete(parse_all_sites(domains, connection))
 
 
 if __name__ == "__main__":
