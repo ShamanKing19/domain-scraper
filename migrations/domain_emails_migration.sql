8f078ce0772c6e7e80c3f3506ca6ba59adf4843f
George <gera.sukhomlin@gmail.com>
2022-06-15 22:02:39 +0200

    Композитный ключ в таблицах domain_emails и domain_phones. При повторной прогонке записи дублироваться не будут

--- migrations/domain_emails_migration.sql
+++ migrations/domain_emails_migration.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `domain_emails` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `domain_id` BIGINT UNSIGNED,
-    `email` TEXT,
-    FOREIGN KEY (`domain_id`) REFERENCES `domains` (`Id`)
+    `email` VARCHAR(255),
+    FOREIGN KEY (`domain_id`) REFERENCES `domains` (`id`),
+    UNIQUE KEY (`domain_id`, `email`)
 );
--- migrations/domain_phones_migration.sql
+++ migrations/domain_phones_migration.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS `domain_phones` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `domain_id` BIGINT UNSIGNED,
     `number` VARCHAR(20),
-    FOREIGN KEY (`domain_id`) REFERENCES `domains` (`id`)
+    FOREIGN KEY (`domain_id`) REFERENCES `domains` (`id`),
+    UNIQUE KEY (`domain_id`, `number`)
+
 );
