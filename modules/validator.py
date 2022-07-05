2c02186f3bae21352f28125a94da56d7f7a93e76
Георгий Сухомлин <georgy.sukhomlin@atos.net>
2022-07-05 16:08:06 +0200

    Рефакторинг в camelCase

--- modules/validator.py
+++ modules/validator.py
@@ -12,32 +12,32 @@ from phonenumbers import carrier
 from phonenumbers import timezone
 import copy
 
-dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
+dotenvPath = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenvPath):
+    load_dotenv(dotenvPath)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 class Validator():
     def __init__(self, categories, regions):
-        self.categories = self.get_categories_with_stripped_tags(categories) 
-        self.compiled_tags_categories = self.get_compiled_tags(copy.deepcopy(categories))
-        self.compiled_status_banwords = self.get_compiled_status_banwords()
+        self.categories = self.GetCategoriesWithStrippedTags(categories) 
+        self.compiledTagsCategories = self.GetCompiledTags(copy.deepcopy(categories))
+        self.compiledStatusBanwords = self.GetCompiledStatusBanwords()
         self.regions = regions
                 
        # TODO: Довести до идеала регулярки
-        self.re_numbers_template = re.compile(r"\+?[78]{1}[\s\(-]{0,2}[0-9]{3,4}[\s\)]?[\s-]?[0-9-\s]{0,4}[0-9-\s]{0,5}")
-        self.re_sub_number_template = re.compile(r"[^0-9]")
-        self.re_emails_template = re.compile(r"[a-zA-Z0-9\.\-_]+@[a-zA-Z0-9_\-]+\.[a-zA-Z]+\.?[a-zA-Z]*\.?[a-zA-Z]*")
+        self.reNumbersTemplate = re.compile(r"\+?[78]{1}[\s\(-]{0,2}[0-9]{3,4}[\s\)]?[\s-]?[0-9-\s]{0,4}[0-9-\s]{0,5}")
+        self.reSubNumberTemplate = re.compile(r"[^0-9]")
+        self.reEmailsTemplate = re.compile(r"[a-zA-Z0-9\.\-_]+@[a-zA-Z0-9_\-]+\.[a-zA-Z]+\.?[a-zA-Z]*\.?[a-zA-Z]*")
 
-        self.re_inn_template = re.compile(r"\b\d{4}\d{6}\d{2}\b|\b\d{4}\d{5}\d{1}\b")
-        self.re_company_template = re.compile(r"\b[ОПАЗНК]{2,3}\b\s+\b\w+\b")
+        self.reInnTemplate = re.compile(r"\b\d{4}\d{6}\d{2}\b|\b\d{4}\d{5}\d{1}\b")
+        self.reCompanyTemplate = re.compile(r"\b[ОПАЗНК]{2,3}\b\s+\b\w+\b")
 
 
     # TODO: Переставить слова местами для оптимизации
-    def get_status_banwords(self):
+    def GetStatusBanwords(self):
         # Ключевые слова для заголовка
-        status_banwords_title = [
+        statusBanwordsTitle = [
             {
                 "name": "Empty",
                 "status": 1000,
@@ -111,7 +111,7 @@ class Validator():
         ]
 
         # Ключевые слова для описания
-        status_banwords_description = [
+        statusBanwordsDescription = [
             {
                 "name": "Empty",
                 "status": 2000,
@@ -172,7 +172,7 @@ class Validator():
         ]
 
         # Ключевые слова для контента
-        status_banwords_content = [
+        statusBanwordsContent = [
             {
                 "name": "Empty",
                 "status": 3000,
@@ -219,56 +219,56 @@ class Validator():
         ]
 
         banwords = {
-            "title": status_banwords_title,
-            "description": status_banwords_description,
-            "content": status_banwords_content,
+            "title": statusBanwordsTitle,
+            "description": statusBanwordsDescription,
+            "content": statusBanwordsContent,
         }
         return banwords
 
 
-    def get_compiled_status_banwords(self):
-        status_banwords = self.get_status_banwords()
-        compiled_status_banwords = {
+    def GetCompiledStatusBanwords(self):
+        statusBanwords = self.GetStatusBanwords()
+        compiledStatusBanwords = {
             "title": [],
             "description": [],
             "content": [],
         }
 
-        for part in compiled_status_banwords.keys():
-            for category in status_banwords[part]:
+        for part in compiledStatusBanwords.keys():
+            for category in statusBanwords[part]:
                 keywords = [re.compile(fr"\b{word.lower()}\b") for word in category["keywords"]]
-                compiled_status_banwords[part].append({
+                compiledStatusBanwords[part].append({
                     "name": category["name"],
                     "status": category["status"],
                     "keywords": keywords
                 })
-        return compiled_status_banwords
+        return compiledStatusBanwords
 
 
-    async def check_invalid_status(self, bs4, title, description, id, url):
-        search_parts = {
+    async def CheckInvalidStatus(self, bs4, title, description, id, url):
+        searchParts = {
             "title": title,
             "description": description,
             "content": bs4.text,
         }
 
-        for part in self.compiled_status_banwords.keys():
-            for category in self.compiled_status_banwords[part]:
+        for part in self.compiledStatusBanwords.keys():
+            for category in self.compiledStatusBanwords[part]:
                 for banword in category["keywords"]:
-                    if re.search(banword, search_parts[part].lower()):
+                    if re.search(banword, searchParts[part].lower()):
                         # print(id, category["name"], category["status"], banword, url, sep=" - ")
                         return [category["status"], banword]
         return [False]
 
 
-    def get_categories_with_stripped_tags(self, categories):
+    def GetCategoriesWithStrippedTags(self, categories):
         for subcategory in categories:
             tags = [tag.strip() for tag in subcategory["tag"].split(",")]
             subcategory["tag"] = tags
         return categories
 
 
-    def get_compiled_tags(self, categories):
+    def GetCompiledTags(self, categories):
         for subcategory in categories:
             tags = []
             # Компиляция тэгов подкатегории
@@ -279,42 +279,42 @@ class Validator():
 
 
     # ! Мегапрожорливый и медленный, но точный
-    async def identify_category(self, bs4, title, description, url):
-        rating_dict = {}
+    async def IdentifyCategory(self, bs4, title, description, url):
+        ratingDict = {}
         # Установки начального рейтинга
-        for subcategory in self.compiled_tags_categories:
-            rating_dict[subcategory["id"]] = 0
+        for subcategory in self.compiledTagsCategories:
+            ratingDict[subcategory["id"]] = 0
         
         s = time.time()
-        for subcategory in self.compiled_tags_categories:
+        for subcategory in self.compiledTagsCategories:
             for tag in subcategory["tag"]:
                 text = title + description + bs4.text
-                rating_dict[subcategory["id"]] += len(re.findall(tag, text))
+                ratingDict[subcategory["id"]] += len(re.findall(tag, text))
         
-        if max(rating_dict) == 0:
+        if max(ratingDict) == 0:
             return 0
         
-        result = max(rating_dict, key=rating_dict.get)  
+        result = max(ratingDict, key=ratingDict.get)  
         print(f"{result} - {time.time() - s}")
         return result
 
 
     # TODO: Можно будет удалить и брать данные из запроса по ИНН
-    async def identify_city_by_inn(self, inns):
-        result_regions = []
+    async def IdentifyCityByInn(self, inns):
+        resultRegions = []
         for inn in inns:
             for region in self.regions:
                 if int(inn[:2]) == region["id"]:
-                    result_regions.append(region["region"].strip())
-        return result_regions
+                    resultRegions.append(region["region"].strip())
+        return resultRegions
 
 
-    async def identify_city_by_number(self, numbers):
+    async def IdentifyCityByNumber(self, numbers):
         cities = []
         try:
             for number in numbers:
-                valid_number = phonenumbers.parse(number, "RU")
-                location = geocoder.description_for_number(valid_number, "ru")
+                validNumber = phonenumbers.parse(number, "RU")
+                location = geocoder.description_for_number(validNumber, "ru")
                 # operator = carrier.name_for_number(valid_number, "en")
                 if location: cities.append(location)
             return list(set(cities))
@@ -325,15 +325,15 @@ class Validator():
     # Бесплатные API для проверки организации по ИНН
     # 1). https://htmlweb.ru/service/organization_api.php#api
     # 2). https://egrul.itsoft.ru/{inn}.json или .xml #* Использую его
-    async def find_inn(self, bs4):
-        all_inns = list(set(re.findall(self.re_inn_template, bs4.text)))
-        correct_inns = []
+    async def FindInn(self, bs4):
+        allInns = list(set(re.findall(self.reInnTemplate, bs4.text)))
+        correctInns = []
         coefficients_10 = [2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
         coefficients_12_1 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0, 0]
         coefficients_12_2 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
         
         # Тут должны быть формулы проверки корректности ИНН
-        for inn in all_inns:
+        for inn in allInns:
             if len(inn) == 10:
                 key = 0
                 # Расчёт ключа по коэффициентам
@@ -342,7 +342,7 @@ class Validator():
                 # key mod 11
                 key_value = key - (key // 11) * 11
                 if key_value == int(inn[-1]):
-                    correct_inns.append(inn)
+                    correctInns.append(inn)
 
             elif len(inn) == 12:
                 key1 = 0
@@ -351,63 +351,63 @@ class Validator():
                 for i, digit in enumerate(inn, start=0):
                     key1 += int(digit) * coefficients_12_1[i]
                 # key mod 11 №1
-                key_value1 = key1 - (key1 // 11) * 11
+                keyValue_1 = key1 - (key1 // 11) * 11
                 # Расчёт ключа №2 по коэффициентам
                 for i, digit in enumerate(inn, start=0):
                     key2 += int(digit) * coefficients_12_2[i]
-                key_value2 = key2 - (key2 // 11) * 11
+                keyValue_2 = key2 - (key2 // 11) * 11
                 # key mod 11 №2
-                if key_value1 == int(inn[10]) and key_value2 == int(inn[11]):
-                    correct_inns.append(inn)
-        if len(correct_inns) > 0:
-            return correct_inns
+                if keyValue_1 == int(inn[10]) and keyValue_2 == int(inn[11]):
+                    correctInns.append(inn)
+        if len(correctInns) > 0:
+            return correctInns
         else:
             return []
 
 
-    async def find_emails(self, bs4):
+    async def FindEmails(self, bs4):
         # Поиск по тексту
-        raw_emails = re.findall(self.re_emails_template, bs4.text)
-        valid_emails = []
+        rawEmails = re.findall(self.reEmailsTemplate, bs4.text)
+        validEmails = []
 
         links = [a for a in bs4.findAll("a", {"href": True}) if "mailto:" in a.get("href")]
         for a in links:
             if a.text:
-                raw_emails.append(a.text)
+                rawEmails.append(a.text)
             else:
-                raw_emails.append(a.get("href").split(":")[-1])
+                rawEmails.append(a.get("href").split(":")[-1])
 
-        for email in raw_emails:
-            matched_email = re.match(self.re_emails_template, email)
-            if matched_email:
-                valid_email = matched_email.string
-                valid_emails.append(valid_email.strip("\"\'.,"))
-        return list(set(valid_emails))
+        for email in rawEmails:
+            matchedEmail = re.match(self.reEmailsTemplate, email)
+            if matchedEmail:
+                validEmail = matchedEmail.string
+                validEmails.append(validEmail.strip("\"\'.,"))
+        return list(set(validEmails))
 
 
-    async def find_phone_numbers(self, bs4):
+    async def FindPhoneNumbers(self, bs4):
         # Поиск номеров в тексте
-        raw_numbers = re.findall(self.re_numbers_template, bs4.text)
-        valid_numbers = []
+        rawNumbers = re.findall(self.reNumbersTemplate, bs4.text)
+        validNumbers = []
 
         # Поиск номеров в тэгах
         links = [a for a in bs4.findAll("a", {"href": True}) if "tel:" in a.get("href")]
         for a in links:
             if a.text:
-                raw_numbers.append(a.text)
+                rawNumbers.append(a.text)
             else:
-                raw_numbers.append(a.get("href").split(":")[-1])
+                rawNumbers.append(a.get("href").split(":")[-1])
 
-        for raw_number in raw_numbers:
-            no_symbols_number = re.sub(self.re_sub_number_template, "", raw_number)
-            if len(no_symbols_number) == 11: 
-                if no_symbols_number[0] == "8" or no_symbols_number[0] == "7":
-                    valid_numbers.append(no_symbols_number)
-        return list(set(valid_numbers))
+        for rawNumber in rawNumbers:
+            noSymbolsNumber = re.sub(self.reSubNumberTemplate, "", rawNumber)
+            if len(noSymbolsNumber) == 11: 
+                if noSymbolsNumber[0] == "8" or noSymbolsNumber[0] == "7":
+                    validNumbers.append(noSymbolsNumber)
+        return list(set(validNumbers))
         
 
-    async def identify_cms(self, html):
-        cms_keywords = {
+    async def IdentifyCms(self, html):
+        cmsKeywords = {
             'src="/bitrix/': "Bitrix",
             '<link href="/bitrix':"Bitrix",
             '/wp-content/themes/':  "Wordpress",
@@ -430,25 +430,25 @@ class Validator():
             'content="CMS EDGESTILE SiteEdit">': 'SiteEdit',
             'meta name="generator" content="OkayCMS': 'Okay'
         }
-        for keyword in cms_keywords:
+        for keyword in cmsKeywords:
             if keyword in html:
-                return cms_keywords[keyword]
+                return cmsKeywords[keyword]
         return "" 
 
 
-    async def find_keywords(self, bs4):
+    async def FindKeywords(self, bs4):
         keywords = bs4.find("meta", {"name": "keywords"})
         if not keywords or "content" not in keywords.attrs.keys(): return ""
         return keywords["content"].replace("\n", "").replace('"', "").replace("'", "").strip()
 
 
-    async def find_description(self, bs4):
+    async def FindDescription(self, bs4):
         description = bs4.find("meta", {"name": "description"})
         if not description or "content" not in description.attrs.keys(): return ""
         return description["content"].replace("\n", "").replace('"', "").replace("'", "").strip()
         
 
-    async def find_title(self, bs4):
+    async def FindTitle(self, bs4):
         title = bs4.find("title")
         if not title: return "" 
         return title.get_text().replace("\n", "").replace('"', "").replace("'", "").strip()
--- scripts/tableCreator.py
+++ scripts/tableCreator.py
@@ -3,89 +3,89 @@ import time
 import pymysql
 
 try:
-    from modules.db_connector import DbConnector
+    from dbConnector import DbConnector
 except:
-    from db_connector import DbConnector
+    from scripts.dbConnector import DbConnector
 
 class TableCreator:
     def __init__(self):
         self.db = DbConnector()
 
 
-    def create_tables(self):
+    def CreateTables(self):
         # domains TABLE creation
         with open("migrations/domains_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())
+            self.db.MakeDbRequest(migration.read())
 
         #  categories TABLE creation
         with open("migrations/categories_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())         
+            self.db.MakeDbRequest(migration.read())         
             
         # categories VALUES creation
         with open("migrations/categories_values_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())                
+            self.db.MakeDbRequest(migration.read())                
         
         # subcategories TABLE creation
         with open("migrations/subcategories_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())         
+            self.db.MakeDbRequest(migration.read())         
             
         # subcategories VALUES creation
         with open("migrations/subcategories_values_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())   
+            self.db.MakeDbRequest(migration.read())   
 
         # tags TABLE creation
         with open("migrations/tags_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())         
+            self.db.MakeDbRequest(migration.read())         
             
         # tags VALUES creation
         with open("migrations/tags_values_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())  
+            self.db.MakeDbRequest(migration.read())  
         
         # domain_info TABLE creation
         with open("migrations/domain_info_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())
+            self.db.MakeDbRequest(migration.read())
         
         # domain_phones TABLE creation
         with open("migrations/domain_phones_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())
+            self.db.MakeDbRequest(migration.read())
 
         # domain_emails TABLE creation
         with open("migrations/domain_emails_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())
+            self.db.MakeDbRequest(migration.read())
 
         # regions TABLE creation
         with open("migrations/regions_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())         
+            self.db.MakeDbRequest(migration.read())         
             
         # regions VALUES creation
         with open("migrations/region_values_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read()) 
+            self.db.MakeDbRequest(migration.read()) 
 
         # company_info TABLE creation
         with open("migrations/company_info_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read()) 
+            self.db.MakeDbRequest(migration.read()) 
         
         # company_finances TABLE creation
         with open("migrations/company_finances_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read()) 
+            self.db.MakeDbRequest(migration.read()) 
         
         # additional_activities_migration TABLE creation
         with open("migrations/additional_activities_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())
+            self.db.MakeDbRequest(migration.read())
         
         # company_additional_activities_migration TABLE creation
         with open("migrations/company_additional_activities_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())
+            self.db.MakeDbRequest(migration.read())
          
         # company_founders_migration TABLE creation
         with open("migrations/company_founders_migration.sql", "r", encoding="utf-8") as migration:
-            self.db.make_db_request(migration.read())
+            self.db.MakeDbRequest(migration.read())
 
 
 
 def main():
     table_creator = TableCreator()
-    table_creator.create_tables()
+    table_creator.CreateTables()
     print("Таблицы созданы и заполнены!")  
 
 
