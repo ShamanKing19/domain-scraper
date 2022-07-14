c48573178fe399f7fd0a0df6fc199f10a6d0dc5f
Георгий Сухомлин <georgy.sukhomlin@atos.net>
2022-07-14 09:20:53 +0200

    Обработка ошибок подключения к бд. Парсер будет ждать пока подключение не восстановится.

--- modules/dbConnector.py
+++ modules/dbConnector.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import os
+import time
 import pymysql
 
 
@@ -9,24 +11,54 @@ class DbConnector:
         self.dbUser = os.environ.get("DB_USER")
         self.dbPassword = os.environ.get("DB_PASSWORD")
         self.connection = self.createConnection()
+        
+
 
     def createConnection(self):
-        connection = pymysql.connect(host=self.dbHost, user=self.dbUser, password=self.dbPassword,
-                                     database=self.dbName, cursorclass=pymysql.cursors.DictCursor)
-        return connection
+        # Для устойчивости от падения бд
+        isStable = False
+        while not isStable:
+            try:
+                connection = pymysql.connect(
+                    host=self.dbHost,
+                    user=self.dbUser,
+                    password=self.dbPassword,
+                    database=self.dbName,
+                    cursorclass=pymysql.cursors.DictCursor
+                )
+                isStable = True
+                return connection
+            # TODO: Обрабатывать только ошибки подключения
+            except pymysql.Error as e:
+                print(f"Connection error: {e} - {datetime.now()}")
+                time.sleep(5)
 
 
     def makeDbRequest(self, sql):
-        with self.connection.cursor() as cursor:
-            cursor.execute(sql)
-            result = cursor.fetchall()
-        self.connection.commit()
-        return result
+        isStable = False
+        while not isStable:
+            try:
+                with self.connection.cursor() as cursor:
+                    cursor.execute(sql)
+                    result = cursor.fetchall()
+                self.connection.commit()
+                isStable = True
+                return result
+            except (pymysql.err.ProgrammingError, pymysql.err.OperationalError) as e:
+                print(f"Request error: {e} - {datetime.now()}")
+                time.sleep(5)
 
 
     def makeSingleDbRequest(self, sql):
-        with self.connection.cursor() as cursor:
-            # Количество url'ов
-            cursor.execute(sql)
-            result = cursor.fetchone()
-        return result
+        isStable = False
+        while not isStable:
+            try:
+                with self.connection.cursor() as cursor:
+                    # Количество url'ов
+                    cursor.execute(sql)
+                    result = cursor.fetchone()
+                    isStable = True
+                return result
+            except (pymysql.err.ProgrammingError, pymysql.err.OperationalError) as e:
+                print(f"Single request error: {e} - {datetime.now()}")
+                time.sleep(5)
